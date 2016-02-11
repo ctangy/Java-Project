a632795ef8baac8ffdfca3d3b2c50a36fcb122dc
Student6 <Student6@10.8.33.147>
2016-02-11 13:48:20 -0500

    Solution

--- Classwork/src/review/Hanoi.java
+++ Classwork/src/review/Hanoi.java
@@ -2,17 +2,18 @@ package review;
 public class Hanoi {
 
 	public static void main(String[] args) {
-		move(1,"A","B","C");//index of the last disc out of w length
+		move(3,"A","B","C");//index of the last disc out of w length
 
 	}
 	
 	public static void move(int n, String from, String mid, String to){
-		if(n == 0){
-			System.out.println("Move a disc "+n+ "from"+ from + "to "+ to);
+		if(n == 1){
+			System.out.println("Move a disc "+n+ " from "+ from + " to "+ to);
 		}
-		if(n > 1){
+		if(n>1){
 			move(n-1, from,to, mid);
-			System.out.println("Move a disc "+n+ "from"+ from + "to "+ to);
+			System.out.println("Move a disc "+n+ " from "+ from + " to "+ to);
+			move(n-1,mid, from, to);
 		}
 	}
 }
