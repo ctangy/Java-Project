004e04d808654b0ff02463522d47c34267ce95d1
Student6 <Student6@10.8.33.118>
2016-02-24 13:44:51 -0500

    2/24/16

---
+++ ClassworkThings/src/HiddenWord.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HiddenWord {
+	 public static String word;
+	
+	public static void main(String[] args) {
+		getHint(word);
+	}
+	
+	public static String getHint(String word){
+		String hint = "";
+		return hint;
+		
+	}
+	
+	public HiddenWord(String word){
+		this.word = word;
+	}
+
+}
