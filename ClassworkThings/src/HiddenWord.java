c3ae13a56f9a6ae5c7ff03c5aeee3976842ae492
Carmen Tangy <ctang9830@bths.edu>
2016-02-26 12:14:45 -0500

    Update HiddenWord.java

--- ClassworkThings/src/HiddenWord.java
+++ ClassworkThings/src/HiddenWord.java
@@ -1,20 +1,30 @@
-import java.util.Scanner;
 
 public class HiddenWord {
+	
 	 public static String word;
 	
 	public static void main(String[] args) {
-		getHint(word);
+		HiddenWord hidden = newh HiddenWord("HELLO");
+		System.out.println(hidden.getHint(HALLO);
 	}
 	
-	public static String getHint(String word){
-		String hint = "";
-		return hint;
+	public static String getHint(String input){
+		String temp="";
+		for(int i=0;i<word.length();i++){
+			String letter= input.substring(i,i+1);
+			if(word.substring(i,i+1).equals(letter)){
+				temp+=letter;
+			}else{
+				if(word.indexOf(letter)>=0){
+					temp+="+";
+				}else{
+					temp+="*";
+				}
+			}
+		return temp;
 		
 	}
 	
 	public HiddenWord(String word){
 		this.word = word;
 	}
-
-}
