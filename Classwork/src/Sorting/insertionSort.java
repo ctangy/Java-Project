6aca743abb2288e317ec2e4012fe81fb12c33efa
Student6 <Student6@10.8.33.118>
2016-03-30 13:57:39 -0400

    insertionSort

---
+++ Classwork/src/Sorting/insertionSort.java
@@ -0,0 +1,31 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class insertionSort {
+
+	public static void main(String[] args) {
+		int[] array =  {4,7,5,2,6,3,1,0};
+		sort(array);
+
+	}
+
+	public static void swap(int i, int[] array, int j){
+		array[j+1] = array[j];
+		j= j-1;
+	}
+	
+	public static void sort(int[] array) {
+		for(int i = 1; i < array.length; i++){
+			int num = array[i];
+			int j = i-1;
+			while( j >= 0 && array[j+1] > num){
+				swap(i, array,j);
+				
+			}
+			array[j+1] = num;
+System.out.println("Switched " +array[i]+ " and " +array[j] +".");
+				System.out.println(Arrays.toString(array));
+		}
+	}
+}
